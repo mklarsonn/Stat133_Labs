a377b7e8debcfdf0621ebb75d63802b985cb1ace
MK Larson <41091252+mklarsonn@users.noreply.github.com>
2018-12-22 13:47:45 -0800

    Add files via upload

---
+++ lab06-mk-larson.R
@@ -0,0 +1,20 @@
+#Lab06
+# with "base" read.csv()
+#cd 
+#cd Documents
+#cd stat133
+#mkdir lab06
+#cd lab06
+#mkdir data
+#mkdir code
+#mkdir output
+#mkdir images 
+#ls
+#cd data
+#curl -O https://raw.githubusercontent.com/ucb-stat133/stat133-fall-2018/master/data/nba2018-players.csv
+#ls
+#wc -l nba2018-players.csv
+#there are 478 lines 
+#head nba2018-players.csv
+#tail nba2018-players.csv 
+
